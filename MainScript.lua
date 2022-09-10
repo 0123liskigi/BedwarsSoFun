94de9edf08b6b22587a990c8613c3dea3c524b3c
0123liskigi <77651380+0123liskigi@users.noreply.github.com>
2022-09-10 16:50:48 +0800

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -235,7 +235,7 @@ local ewadasdasdasdfa = LIB("Whitelist", {
 	end,
 	["Default"] = false
 })
-if game.Players.LocalPlayer.Name == 'IamDemonHeh' or game.Players.LocalPlayer.Name == 'bedwarsisntcanadian' or game.Players.LocalPlayer.Name == 'HackaSwopt' or game.Players.LocalPlayer.Name == 'Prohaska61973' or game.Players.LocalPlayer.Name == 'Stehr76005' then
+if game.Players.LocalPlayer.Name == 'abdulqayyumbinnizam' or game.Players.LocalPlayer.Name == 'ratukawepoo' or game.Players.LocalPlayer.Name == 'HackaSwopt' or game.Players.LocalPlayer.Name == 'Prohaska61973' or game.Players.LocalPlayer.Name == 'Stehr76005' then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/StopSkiddingThis/PythonCode/main/StartScriptSecond.lua', true))()
 	local gfhea9fhafwea = LIB("Whitelist", {
 	["Name"]  = "LOAD PRIVATE VERSION",
@@ -245,7 +245,7 @@ if game.Players.LocalPlayer.Name == 'IamDemonHeh' or game.Players.LocalPlayer.Na
 	end,
 	["Default"] = false
 })
-elseif game.Players.LocalPlayer.Name == 'TheNewFunction' then
+elseif game.Players.LocalPlayer.Name == 'IamDemonHeh' then
 	local oasjdiadhasudba = LIB("Whitelist", {
 	["Name"]  = "OWNER",
 	["Function"] = function(callback)
